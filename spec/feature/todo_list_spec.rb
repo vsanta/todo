d97109d589ddc072b7c1fb3a52c1de7ec049ba7a
Vanessa Sant'Anna <pair+vanessa.santanna@gmail.com>
2014-09-23 16:51:50 -0400

    remove test for create since it's covered by edit's flow

--- spec/feature/todo_list_spec.rb
+++ spec/feature/todo_list_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
-feature 'Create todo list', js: true do
-  scenario 'user adds an item to the list' do
-    visit '/'
-    fill_in 'new task', with: 'add some test coverage'
-
-    click_on 'add task'
-
-    within '.tasks' do
-      expect(page).to have_text 'add some test coverage'
-      expect(page).to have_text 'incomplete'
-    end
-  end
-end
-
 feature 'Edit todo list', js: true do
   scenario 'user marks a task as complete' do
     visit '/'
